cc0973e8f69a6f4f9ddb83262e5272a2db52f198
Guy-Frank Essome Penda <guyfrank@outlook.com>
2021-12-16 19:56:49 -0500

    implement insert and select in client

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import asyncio
 import boto3
+import pickle
+import multiprocessing
 
 client = boto3.client('ec2')
 
@@ -17,6 +19,34 @@ proxy_ip = [i['PublicIpAddress'] for r in response['Reservations'] for i in r['I
 HOST = proxy_ip  # The server's hostname or IP address
 PORT = 5001        # The port used by the server
 
+mode = 'direct hit'
+
+async def run_insert_person(reader, writer, person):
+    seq, first, last, age, city, phone, email, street, birthday, gender = person.split(',')
+    statement = f'INSERT INTO people VALUES ({seq}, {first}, {last}, {age}, {city}, \'{phone}\', \'{email}\', {street}, \'{birthday}\', {gender});'
+    
+    obj = {'type': 'insert', 'statement': statement}
+    pickledobj = pickle.dumps(obj)
+    writer.write(pickledobj)
+    await writer.drain()
+
+    # confirm insertion
+    data = await reader.read(1024)
+    print(f'Received: {data.decode()!r}')
+
+async def run_read_person(reader, writer, person):
+    seq = person.split(',')[0]
+    statement = f'SELECT seq, first, last FROM people WHERE seq = {seq};'
+    
+    obj = {'type': 'select', 'statement': statement}
+    pickledobj = pickle.dumps(obj)
+    writer.write(pickledobj)
+    await writer.drain()
+
+    # confirm insertion
+    data = await reader.read(1024)
+    print(f'Received: {data.decode()!r}')
+
 async def tcp_client():
 
     while True:
@@ -25,21 +55,37 @@ async def tcp_client():
 
             reader, writer = await asyncio.open_connection(HOST, PORT)
             
-            print('Connected to proxy!')
+            print('Connected!')
             break
         except ConnectionRefusedError:
             print('Trying again in 15 seconds...')
             await asyncio.sleep(15)
             pass
 
+    # read data file
+    with open('people.csv', 'r') as f:
+        people = f.readlines()
+
     user_input = input('Send: ')
 
     while user_input != '':
-        writer.write(user_input.encode())
-        await writer.drain()
 
-        data = await reader.read(1024)
-        print(f'Received: {data.decode()!r}')
+        if user_input == 'insert':
+            # insert data to db
+            for person in people:
+                run_insert_person(reader, writer, person)
+
+        elif user_input == 'select':
+            # retrieve data from db
+            for person in people:
+                run_read_person(reader, writer, person)
+
+        else:
+            writer.write(user_input.encode())
+            await writer.drain()
+
+            data = await reader.read(1024)
+            print(f'Received: {data.decode()!r}')
 
         user_input = input('Send: ')
 
